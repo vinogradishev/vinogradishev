2a54216d4282df422ca8435f2eca3ebc452b06ee
vinogradishev <102917821+vinogradishev@users.noreply.github.com>
2023-07-19 19:59:31 +0500

    Add Stack class.
    Stack class with List.

---
+++ StackOtus/StackOtus/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace StackOtus
+{
+    class TestStackMethods
+    {
+        static void Main()
+        {
+            var s = new Stack("a", "b", "c");
+            // size = 3, Top = 'c'
+            Console.WriteLine($"size = {s.Size}, Top = '{s.Top}'");
+            var deleted = s.Pop();
+            // Извлек верхний элемент 'c' Size = 2
+            Console.WriteLine($"Извлек верхний элемент '{deleted}' Size = {s.Size}");
+            s.Add("d");
+            // size = 3, Top = 'd'
+            Console.WriteLine($"size = {s.Size}, Top = '{s.Top}'");
+            s.Pop();
+            s.Pop();
+            s.Pop();
+            // size = 0, Top = null
+            Console.WriteLine($"size = {s.Size}, Top = {(s.Top == null ? "null" : s.Top)}");
+            s.Pop();
+            var a = new Stack("a", "b", "c", "d", "e");
+            a.Merge(new Stack("1", "2", "3", "4", "5"));
+
+            foreach (var x in a.stackProp)
+            {
+                Console.Write(x + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("-------------------------------");
+
+            s = Stack.Concat(new Stack("a", "b", "c"), new Stack("1", "2", "3"), new Stack("А", "Б", "В"));
+            foreach (var x in s.stackProp)
+            {
+                Console.Write(x + " ");
+            }
+        }
+    }
+}
---
+++ StackOtus/StackOtus/StackExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace StackOtus
+{
+    internal static class StackExtensions
+    {
+        static List<string>? _mergeStackList1;
+
+        public static void Merge(this Stack stack1, Stack stack2)
+        {
+            int stackSize = stack2.Size;
+            for (int i = 0; i < stackSize; i++)
+            {
+                string insertStr = stack2.Pop();
+                stack1.Add(insertStr);
+            }
+        }
+    }
+}
