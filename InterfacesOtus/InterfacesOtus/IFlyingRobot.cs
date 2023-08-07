dd96f2a39b26ce0f89410d056978c0793c1be52d
vinogradishev <102917821+vinogradishev@users.noreply.github.com>
2023-08-07 20:18:51 +0500

    Updated home work 5.

---
+++ InterfacesOtus/InterfacesOtus/IFlyingRobot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesOtus
+{
+    internal interface IFlyingRobot : IRobot
+    {
+        string IRobot.GetRobotType() 
+        {
+            return "I am a flying robot.";
+        }
+    }
+}
