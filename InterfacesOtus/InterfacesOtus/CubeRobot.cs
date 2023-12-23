37382c1712d9cd7e965207ae0b5e3f3ebacfa4c3
vinogradishev <102917821+vinogradishev@users.noreply.github.com>
2023-12-23 09:48:48 +0500

    Add files via upload

---
+++ InterfacesOtus/InterfacesOtus/CubeRobot.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesOtus
+{
+    internal class CubeRobot : IRobot, IChargeable
+    {
+        List<string> _list_components;
+
+        bool _charged = false;
+
+        string Charged
+        {
+            get
+            {
+                if (_charged)
+                {
+                    return "I am charged";
+                }
+                else
+                {
+                    return "I am not charged";
+                }
+            }
+        }
+        public string GetInfo()
+        {
+            return "I am a simple robot with 2 hands and 2 wheels.";
+        }
+        string IChargeable.GetInfo()
+        {
+            return Charged;
+        }
+        public List<string> GetComponents()
+        {
+            return _list_components;
+        }
+        public void Charge()
+        {
+            Console.WriteLine("Charging...");
+            Thread.Sleep(3000);
+            Console.WriteLine("Cherged");
+            _charged = true;
+        }
+        public string GetRobotType()
+        {
+            return "I am a simple robot.";
+        }
+        public CubeRobot() 
+        {
+            _list_components = new List<string> { "wheel1", "wheel2", "hand1", "hand2" };
+        }
+    }
+}
---
+++ InterfacesOtus/InterfacesOtus/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace InterfacesOtus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Quadcopter quad1 = new Quadcopter();
+            
+            Console.WriteLine(quad1.GetRobotType());
+            Console.WriteLine(((IChargeable)quad1).GetInfo());
+            quad1.Charge();
+            Console.WriteLine(((IChargeable)quad1).GetInfo());
+            Console.WriteLine(quad1.GetInfo());
+            Console.WriteLine("----------------------------");
+            
+            CubeRobot cube1 = new CubeRobot();
+            
+            Console.WriteLine(cube1.GetRobotType());
+            Console.WriteLine(((IChargeable)cube1).GetInfo());
+            cube1.Charge();
+            Console.WriteLine(((IChargeable)cube1).GetInfo());
+            Console.WriteLine(cube1.GetInfo());
+            
+        }
+    }
+}
